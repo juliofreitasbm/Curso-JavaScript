908f69c5a727a10de6e791c474ae8dcb1da76fcc
Julio <juliofreitas.bm@gmail.com>
2020-05-25 22:57:48 -0300

    Exercícios JavaScript Parte 7 - ex018 correção incompleta

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,51 +1,30 @@
-var primeira_adicao = false
-var vet = []
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+	if(Number(n) >= 1 && Number(n) <= 100) {
+		return true
+	} else {
+		return false
+	}
+}
+
+function inLista(n, l) {
+	if(l.indexOf(Number(n)) != -1) {
+		return true
+	} else {
+		return false
+	}
+}
+
 
 function adicionar() {
-	let num = document.getElementById('txtn')
-	let tab = document.getElementById('seltab')
-
-	if(num.value.length == 0){
-		window.alert('Nenhum número para ser adicionado!')
-	} else if(num.value < 1 || num.value > 100) {
-		window.alert('[ERRO] Número inserido inválido! Digite um número entre 1 e 100')
-	} else if(vet.indexOf(Number(num.value)) != -1) {
-		window.alert('[ERRO] Número já existe na Lista')
-	} else {	
-		if(primeira_adicao == false){
-			tab.innerHTML = ''
-			primeira_adicao = true
-		}
-		let n = Number(num.value)
-		vet.push(n)
-		let item = document.createElement('option')
-		item.text = `Valor ${n} adicionado.`
-		tab.appendChild(item)
+	if(isNumero(num.value) && !inLista(num.value, valores)) {
+		window.alert('Tudo Ok')
+	} else {
+		window.alert('Valor inválido ou já encontrado na lista.')
 	}
 
-}
-
-function finalizar() {
-	let dres = document.getElementById('res')
-	let item2 = document.createElement('p')
-	dres.appendChild(item2)		
-	//console.log(vet)
-	//console.log(typeof vet[0])
-	vet.sort((a,b)=> a-b)
-
-	//console.log(vet)
-	item2.innerHTML = `Ao todo, temos ${vet.length} números cadastrados<br>
-				O maior valor informado foi ${vet[vet.length - 1]}<br>
-				O menor valor informado foi ${vet[0]}<br>
-				Somando todos os valores, temos ${somar(vet)}<br>
-				A média dos valores digitados é ${Math.round(somar(vet)/vet.length)}`
-
-}
-
-function somar(vet) {
-	let soma=0
-	for(let c=0; c<vet.length; c++){
-		soma += vet[c]
-	}
-	return soma
 }
